f8dfb4a8b50b0989c37917962be7a65cdf033296
AkaiSara <sara.righetto@gmail.com>
2019-08-01 21:44:43 +0200

    modificato container

--- container.h
+++ container.h
@@ -10,21 +10,21 @@ un tipo che supporti la clonazione e la distruziona polimorfa. -> virtual ~T(), 
 */ 
 #ifndef CONTAINER_H
 #define CONTAINER_H
-#include "deepptr.h"
+#include "exception.h"
 
 template<class T>
 class Container{ //contenitore, double linked list
 private:
 	class Node{
 		public:
-		DeepPtr<T> info;
+		T info;
 		Node *prev, *next;
-        Node(const DeepPtr<T> &, Node* = nullptr, Node* = nullptr); //non serve nessun valore di default per il primo parametro perchè chiama la funzione apposira in DeepPtr
+        Node(const T & = T(), Node* = nullptr, Node* = nullptr);
 		~Node();
         bool operator==(const Node &) const;
         bool operator!=(const Node &) const;
 	};
-    static Node* copy(Node* f, Node*& l);
+    static Node* copy(Node*, Node*&);
 	
 public:
 	class Iterator{ //template annidato associato
@@ -38,8 +38,8 @@ public:
 		Iterator& operator--();//prefisso
 		Iterator operator--(int); //postfisso
 
-		bool operator==(const Iterator & it) const; //uguaglianza
-		bool operator!=(const Iterator & it) const; //disuguaglianza
+		bool operator==(const Iterator &) const; //uguaglianza
+		bool operator!=(const Iterator &) const; //disuguaglianza
 
 		T& operator*() const; //dereferenziazione
 		T* operator->() const; //accesso a membro
@@ -56,8 +56,8 @@ public:
 		Const_Iterator& operator--();//prefisso
 		Const_Iterator operator--(int); //postfisso
 
-		bool operator==(const Const_Iterator & it) const; //uguaglianza
-		bool operator!=(const Const_Iterator & it) const; //disuguaglianza
+		bool operator==(const Const_Iterator &) const; //uguaglianza
+		bool operator!=(const Const_Iterator &) const; //disuguaglianza
 
 		T& operator*() const; //dereferenziazione
 		T* operator->() const; //accesso a membro
@@ -71,10 +71,8 @@ private: //------campi dati contenitore-----
 public:
 	//---contenitore----
     Container(); //costruttore di default
-    Container(unsigned int, const T & = T());
-    Container(unsigned int);
-	Container(const Container &); //costruttore di copia
-	Container& operator=(const Container & c); //assegnazione
+    Container(const Container &); //costruttore di copia
+	Container& operator=(const Container &); //assegnazione
 	~Container(); //distruttore
 
 	unsigned int getSize() const;
@@ -90,16 +88,12 @@ public:
 	Const_Iterator end() const;
 	Const_Iterator cend() const;
 
-    Iterator insert(const Iterator &, const T &);
-    void insert(Iterator, unsigned int, const T&);
 	void push_back(const T &);
 	void push_front(const T &);
 
-	Iterator erase(Iterator);
-	Iterator erase(Iterator, Iterator);
 	void clear();
-	void pop_back();
-	void pop_front();
+	T pop_back();
+	T pop_front();
 
     Iterator search(const T&);
     Const_Iterator search(const T&) const;
@@ -111,11 +105,12 @@ public:
 //------------node---------
 
 template<class T>
-Container<T>::Node::Node(const DeepPtr<T> & t, Node* p, Node* n) : info(t), prev(p), next(n) {}//costuttore
+Container<T>::Node::Node(const T & t, Node* p, Node* n) : info(t), prev(p), next(n) {}//costuttore
 
 template<class T>
 Container<T>::Node::~Node() {
-	delete next;
+	if(next != nullptr) 
+        delete next;
 }
 
 template<class T>
@@ -145,10 +140,12 @@ Container<T>::Iterator::Iterator(Node * p) : pos(p) {} //costruttore
 
 template<class T>
 typename Container<T>::Iterator& Container<T>::Iterator::operator++(){ //prefisso
-	if(pos != nullptr && pos->next != nullptr)
-		pos = pos-> next;
+    if(pos == nullptr) 
+        throw Exception("invalid iterator");
+    pos = pos-> next;
 	return *this;
 }
+
 template<class T>
 typename Container<T>::Iterator Container<T>::Iterator::operator++(int){ //postfisso
 	Iterator aux(*this);
@@ -157,10 +154,12 @@ typename Container<T>::Iterator Container<T>::Iterator::operator++(int){ //postf
 }
 template<class T>
 typename Container<T>::Iterator& Container<T>::Iterator::operator--(){//prefisso
-	if(pos != nullptr && pos->prev != nullptr)
-		pos = pos->prev;
+    if(pos == nullptr ) 
+        throw Exception("invalid iterator");
+    pos = pos->prev;
 	return *this;
 }
+
 template<class T>
 typename Container<T>::Iterator Container<T>::Iterator::operator--(int){ //postfisso
 	Iterator aux(*this);
@@ -180,14 +179,16 @@ bool Container<T>::Iterator::operator!=(const Iterator & it) const {
 
 template<class T>
 T& Container<T>::Iterator::operator*() const { //dereferenziazione
-	if(pos != nullptr)
-        return *(pos->info); //se contiene qualcosa
+    if(pos == nullptr)
+        throw Exception("invalid iterator");
+    return pos->info;
 }
 
 template<class T>
 T* Container<T>::Iterator::operator->() const {
-	if(pos != nullptr)
-        return pos->info; //se contiene qualcosa
+    if(pos == nullptr)
+        throw Exception("invalid iterator");
+    return  *(pos->info);
 }
 
 template<class T>
@@ -195,8 +196,9 @@ Container<T>::Const_Iterator::Const_Iterator(Node * p) : pos(p) {} //costruttore
 
 template<class T>
 typename Container<T>::Const_Iterator& Container<T>::Const_Iterator::operator++(){ //prefisso
-    if(pos != nullptr && pos->next != nullptr)
-        pos = pos-> next;
+    if(pos == nullptr ) 
+        throw Exception("invalid iterator");
+    pos = pos-> next;
     return *this;
 }
 template<class T>
@@ -207,8 +209,9 @@ typename Container<T>::Const_Iterator Container<T>::Const_Iterator::operator++(i
 }
 template<class T>
 typename Container<T>::Const_Iterator& Container<T>::Const_Iterator::operator--(){//prefisso
-    if(pos != nullptr && pos->prev != nullptr)
-        pos = pos->prev;
+    if(pos == nullptr ) 
+        throw Exception("invalid iterator");
+    pos = pos->prev;
     return *this;
 }
 template<class T>
@@ -230,16 +233,17 @@ bool Container<T>::Const_Iterator::operator!=(const Const_Iterator & it) const {
 
 template<class T>
 T& Container<T>::Const_Iterator::operator*() const { //dereferenziazione
-    if(pos != nullptr)
-        return *(pos->info); //se contiene qualcosa
+    if(pos == nullptr)
+        throw Exception("invalid iterator");
+    return pos->info; 
 }
 
 template<class T>
 T* Container<T>::Const_Iterator::operator->() const {
-    if(pos != nullptr)
-        return &(pos->info); //se contiene qualcosa
+    if(pos == nullptr)
+        throw Exception("invalid iterator");
+    return *(pos->info);
 }
-
 
 
 
@@ -263,22 +267,6 @@ typename Container<T>::Node* Container<T>::copy(Node* f, Node*& l){
 
 template<class T>
 Container<T>::Container() : first(nullptr), last(first), size(0) {}
-	
-//costruisce c con n copie inizializzate a t
-template<class T>
-Container<T>::Container(unsigned int n, const T & t){
-	Container();
-    while(n > 0){
-		push_back(t);
-        n--;
-	}
-}
-
-//costruisce c con n elementi con valore di default -> T deve avere un costruttore di default
-template<class T>
-Container<T>::Container(unsigned int n){
-    Container(n, T());
-}
 
 template<class T>
 Container<T>::Container(const Container & c): first(nullptr), last(nullptr), size(c.size) {
@@ -288,7 +276,8 @@ Container<T>::Container(const Container & c): first(nullptr), last(nullptr), siz
 template<class T>
 Container<T>& Container<T>::operator=(const Container & c){
     if(this != &c){
-        if (first != nullptr) delete first;
+        if (first != nullptr) 
+            delete first;
         size = c.size;
         first = copy(c.first, last);
     }
@@ -300,94 +289,86 @@ Container<T>::~Container(){
     delete first; //chiama ~nodo, quindi la cancella tutta
 }
 
-//inserisce l'elemento t nella sequenza c prima dell'elemento puntato da it e ritorna un iteratore che punta all'elemento appena inserito
-template<class T>
-typename Container<T>::Iterator Container<T>::insert(const Iterator & it, const T & t){
-    if (first == nullptr){ //se vuota
-        last = first = new Node(DeepPtr<T>(&t));
-        size = 1;
-        return Iterator(first);
-    }
-    //se non vuota
-    Node* aux = new Node(DeepPtr<T>(&t), it.pos->prev , it.pos);
-
-    (it.pos != first) ? it.pos->prev->next = aux : first = aux;
-    (it.pos != last) ? it.pos-> prev = aux : last = aux;
-
-    size++;
-    return it.pos->prev;
-}
-
-//inseriscce n copie dell'elemento t prima dell'elemento puntato da it
-template<class T>
-void Container<T>::insert(Iterator it, unsigned int n, const T& t){
-    for(unsigned int i=0; i<n; i++)
-        insert(it,t);
-}
 
 template<class T>
 void Container<T>::push_front(const T & t){
-    insert(begin(), t);
-} //-> c.insert(c.begin(), t)
+    if (first == nullptr){
+        first = last = new Node(t);
+        size = 1;
+    }
+    else {
+        first->prev = new Node(t, nullptr, first);
+        first = first->prev;
+        size++;
+    }
+} 
 
 template<class T>
 void Container<T>::push_back(const T & t){
-    insert(end(), t);
-} //-> c.insert(c.end(), t)
+    if (first == nullptr) {
+        first = last = new Node(t);
+        size = 1;
+        }
+    else {
+        last->next = new Node(t, last);
+        last = last->next;
+        size++;
+    }    
+} 
 
-// distrugge l'elemento puntato da it e ritorna l'iteratore all'elemento successivo
-template<class T>
-typename Container<T>::Iterator Container<T>::erase(Iterator it){
-    if(first == nullptr){ //se vuota
-        //eccezione, non c'è nulla da eliminare
-        return nullptr;
-    }
-    //se non vuota
-    Node * p = it.pos->next != nullptr ? it.pos->next : nullptr;
-    if(first != last) {
-        first != it.pos ? it.pos->prev->next = p : first = p;
-        last != it.pos ? it.pos->next->prev = it.pos->prev: last = it.pos->prev;
-
-        it.pos->next = it.pos->prev = nullptr;
-    }
-    delete it.pos;
-    size--;
-    return Iterator(p);
-}
-
-//distrugge gli elementi nell'intervallo itb - ite, incluso il primo, escluso l'ultimo e ritorna l'elemento successivo all'ultimo rimosso
-template<class T>
-typename Container<T>::Iterator Container<T>::erase(Iterator itb, Iterator ite){
-    while(itb != ite){
-        erase(itb);
-        itb++;
-    }
-    return ite;
-}
-
-//equivale a erase(c.begin(), c.end())
 template<class T>
 void Container<T>::clear(){
-    erase(begin(), end());
+    delete first;
+    first = last = nullptr;
+} 
+
+template<class T>
+T Container<T>::pop_back(){
+    if(first == nullptr)
+        throw Exception("aouifncodsn");
+    /* Node * p = last;
+    T i = p->info;
+    if(first != last){
+        last = last->prev;
+        p->prev = nullptr;
+    }
+    first->prev = last->next = nullptr;
+    delete p;
+    size--;
+    return i; */
+    Node * p = last;
+    T i = p->info;
+    last = last->prev;
+    if(last) 
+        last->next = nullptr;
+    else 
+        first = nullptr;
+    delete p;
+    return i;
+} 
+
+template<class T>
+T Container<T>::pop_front(){
+    if(first == nullptr)
+        throw Exception("zxdb");
+    Node * p = first;
+    T i = p->info;
+    first = first->next;
+    if(first) 
+        first->prev = nullptr;
+    else 
+        last = nullptr;
+    p->next = nullptr;
+    delete p;
+    return i;
 }
-
 template<class T>
-void Container<T>::pop_back(){
-    erase(--end());
-} //-> c.erase(--c.end())
-
-template<class T>
-void Container<T>::pop_front(){
-    erase(begin());
-} //rimuove l'elemento in testa -> c.erase(c.begin())
-
-template<class T>
-T& Container<T>::front()const {
+T& Container<T>::front() const {
 	return *(first->info);
 }
 
 template<class T>
-T& Container<T>::back()const {
+T& Container<T>::back() const {
 	return *(last->info);
 }
 
@@ -398,7 +379,7 @@ typename Container<T>::Iterator Container<T>::begin() {
 
 template<class T>
 typename Container<T>::Iterator Container<T>::end() {
-	return Iterator(nullptr); //last->next
+	return Iterator();
 }
 
 template<class T>
@@ -408,7 +389,7 @@ typename Container<T>::Const_Iterator Container<T>::begin() const {
 
 template<class T>
 typename Container<T>::Const_Iterator Container<T>::end() const {
-	return Const_Iterator(last->next);
+	return Const_Iterator();
 }
 
 template<class T>
@@ -418,7 +399,7 @@ typename Container<T>::Const_Iterator Container<T>::cbegin() const {
 
 template<class T>
 typename Container<T>::Const_Iterator Container<T>::cend() const {
-	return Const_Iterator(last->next);
+	return Const_Iterator();
 }
 
 template<class T>
